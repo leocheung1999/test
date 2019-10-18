b3d021d5bb5afe2ab967be5a638aa8070a3b8729
117326763 <117326763@BISG16STU.ucc-student-it.ad.ucc.ie>
2019-10-18 13:42:18 +0100

    newcode

---
+++ src/testwithgit/NewClass.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testwithgit;
+
+/**
+ *
+ * @author 117326763
+ */
+public class NewClass {
+    private int int1,int2;
+    
+    public NewClass(int int1, int int2){
+        int1 = int1;
+        int2 = int2;
+        
+        
+    }
+
+    public int getInt1() {
+        return int1;
+    }
+
+    public void setInt1(int int1) {
+        this.int1 = int1;
+    }
+
+    public int getInt2() {
+        return int2;
+    }
+
+    public void setInt2(int int2) {
+        this.int2 = int2;
+    }
+    
+}
